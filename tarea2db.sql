cdbf078b278baa153c494caacc5c665e5135e12f
Mayaridz <maryorimayari@gmail.com>
2024-09-09 19:19:55 -0600

    Add files via upload

---
+++ tarea2db.sql
@@ -0,0 +1,9 @@
+/*
+-- Query: SELECT * FROM db_empresa.puestos
+LIMIT 0, 1000
+
+-- Date: 2024-09-09 19:14
+*/
+INSERT INTO `` (`id_puesto`,`puesto`) VALUES (1,'Programador');
+INSERT INTO `` (`id_puesto`,`puesto`) VALUES (2,'Analista');
+INSERT INTO `` (`id_puesto`,`puesto`) VALUES (3,'Administrador');
